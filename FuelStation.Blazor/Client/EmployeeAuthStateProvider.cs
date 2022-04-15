2ff246558832abf8b7f5efae23b662a7d180f167
Stergios Vogiatzis <tehvog@gmail.com>
2022-04-15 15:08:06 +0300

    A try for login and authorization

---
+++ FuelStation.Blazor/Client/EmployeeAuthStateProvider.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace FuelStation.Blazor.Client
+{
+    public class EmployeeAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+        public EmployeeAuthStateProvider(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var state = new AuthenticationState(new ClaimsPrincipal());
+            string username = await _localStorage.GetItemAsStringAsync("username");
+            if (!string.IsNullOrEmpty(username))
+            {
+                var identity = new ClaimsIdentity(new []{
+                    new Claim(ClaimTypes.Name, username)
+                });
+                state = new AuthenticationState(new ClaimsPrincipal(identity));
+            }
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+            return state;
+        }
+    }
+}
--- FuelStation.Blazor/Client/Program.cs
+++ FuelStation.Blazor/Client/Program.cs
@@ -1,4 +1,6 @@
+using Blazored.LocalStorage;
 using FuelStation.Blazor.Client;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +9,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, EmployeeAuthStateProvider>();
 
 await builder.Build().RunAsync();
